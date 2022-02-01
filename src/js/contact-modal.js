8da81ee47e7c40ad0b721d07d85c01457322d96a
Ann Sarviro <Annka_nuha@i.ua>
2022-02-01 14:23:09 +0200

    Открытие модалки под другим именем

--- src/js/contact-modal.js
+++ src/js/contact-modal.js
@@ -1,12 +1,14 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-contact-modal-open]'),
+    openModaBtn: document.querySelector('[data-contact-franchise-modal-open]'),
     closeModalBtn: document.querySelector('[data-contact-modal-close]'),
     contactModal: document.querySelector('[data-contact-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModaFr);
+  refs.openModaBtn.addEventListener('click', toggleModal);
 
 document.addEventListener('touchstart', onTouchStart, {passive: true});
 
@@ -14,4 +16,12 @@ document.addEventListener('touchstart', onTouchStart, {passive: true});
     document.body.classList.toggle('contact-modal-open');
     refs.contactModal.classList.toggle('visually-hidden');
   }
+
+  function toggleModaFr() {
+    document.body.classList.toggle('contact-modal-open');
+    refs.contactModal.classList.toggle('visually-hidden');
+    let paragraph = document.querySelector('.contact-modal__title');
+    paragraph.textContent = 'Franchise!';    
+  }
+  
 })();
