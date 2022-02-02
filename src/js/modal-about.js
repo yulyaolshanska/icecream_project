d2e8f4dbb4f63fca88ac0861dc6229743bcb1871
Max-Loktionov <studreno@gmail.com>
2022-02-02 12:22:16 +0200

    add modal-about
    модалка для продуктов

---
+++ src/js/modal-about.js
@@ -0,0 +1,24 @@
+(() => {
+    const refs = {
+     
+        
+      openModalAboutMenu: document.querySelector('.modal-about-menu--open'),
+      closeModalAboutMenu: document.querySelector('.modal-about-menu--close'),
+      modalAboutMenu: document.querySelector('.modal-about-menu__offer'),
+    
+      };
+  
+  
+    
+    refs.openModalAboutMenu.addEventListener('click', toggleModal);
+    refs.closeModalAboutMenu.addEventListener('click', toggleModal);
+  
+   
+  
+    function toggleModal() {
+     
+      refs.modalAboutMenu.classList.toggle('visually-hidden');
+      document.body.classList.toggle('modal-about-menu--visible');
+    }
+  
+  })();
