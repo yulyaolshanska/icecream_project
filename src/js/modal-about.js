b33163f1b979ed8f785c4b57668dc03955857bcf
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-02 00:28:53 +0300

    fix modal about for iphone

--- src/js/modal-about.js
+++ src/js/modal-about.js
@@ -1,26 +1,17 @@
-"use strict";
+'use strict';
 // Это комментарий. Дальше идет весь код JS-файла
 (() => {
-    const refs = {
-     
-        
-      openModalAboutMenu: document.querySelector('.modal-about-menu--open'),
-      closeModalAboutMenu: document.querySelector('.modal-about-menu--close'),
-      modalAboutMenu: document.querySelector('.modal-about-menu__offer'),
-    
-      };
-  
-  
-    
-    refs.openModalAboutMenu.addEventListener('click', toggleModal);
-    refs.closeModalAboutMenu.addEventListener('click', toggleModal);
-  
-   
-  
-    function toggleModal() {
-     
-      refs.modalAboutMenu.classList.toggle('visually-hidden');
-      document.body.classList.toggle('modal-about-menu--visible');
-    }
-  
-  })();
+  const refs = {
+    openModalAboutMenu: document.querySelector('.modal-about-menu--open'),
+    closeModalAboutMenu: document.querySelector('.modal-about-menu--close'),
+    modalAboutMenu: document.querySelector('.modal-about-menu__offer'),
+  };
+
+  refs.openModalAboutMenu.addEventListener('click', toggleModal);
+  refs.closeModalAboutMenu.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modalAboutMenu.classList.toggle('modal-about__backdrop--hidden');
+    document.body.classList.toggle('modal-about-menu--visible');
+  }
+})();
